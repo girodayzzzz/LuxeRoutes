5f842ef3d81ab00e400cd2b2de45d00a11be7005
girodayzzzz <info.dayzzzz@gmail.com>
2026-06-15 18:54:25 +0000

    Remove duplicate password profile migration

--- migrations/0008_profile_password_fields.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE profiles ADD COLUMN password_hash TEXT;
-ALTER TABLE profiles ADD COLUMN password_salt TEXT;
-ALTER TABLE profiles ADD COLUMN password_iterations INTEGER;
-ALTER TABLE profiles ADD COLUMN password_enabled INTEGER NOT NULL DEFAULT 0;
